9f5b179f63676f258bb5a94d7cfa9dd38ed1506a
nail <nail20142511@gmail.com>
2020-12-18 00:21:12 +0200

    Конец данных

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(thebbc.ru)
+domains=(email.ru)
 rsa_key_size=4096
 data_path="./certbot"
-email="support@thebbc.ru" # Adding a valid address is strongly recommended
+email="support@email" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
